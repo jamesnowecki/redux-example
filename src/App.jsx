01fa298097fc12c5eab606bf7450f372db09b8ea
James Nowecki | Minstersoft <james.nowecki@minstersoft.co.uk>
2020-08-24 13:48:09 +0100

    redux basics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
-
-
+import store from "../src/store/store";
 const App = () => {
   return (
-    <div className={styles.App}>
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className={styles.App}>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
