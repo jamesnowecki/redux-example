a58eada0b78e572d400ee7116db797b6586501bd
James Nowecki | Minstersoft <james.nowecki@minstersoft.co.uk>
2020-08-24 10:04:35 +0100

    basic react functionality complete prior to redux introduction

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import logo from './logo.svg';
 import styles from './App.module.scss';
 
 import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+
 
 const App = () => {
   return (
     <div className={styles.App}>
+      <PostForm />
+      <hr />
       <Posts />
     </div>
   );
